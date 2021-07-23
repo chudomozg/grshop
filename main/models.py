92a0dd87b47130577d6aaaffe5091eed3df1ab36
chudomozg <chudomozg@gmail.com>
2021-07-23 17:19:31 +1000

    add costomer model

--- main/models.py
+++ main/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey  # needs for hierarchical tree of categories
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-#
-# Product
-#
-# Cart
-# CartProduct
+User = get_user_model()
+
+
 # Customer - User extended
 # Promo
-# Order?
+# Order
 
 class Category(MPTTModel):
     # Category - Categories on catalog
@@ -43,6 +42,9 @@ class Product(models.Model):
                               blank=True,
                               verbose_name="Image")
     description = models.TextField(verbose_name="Description")
+    short_desc = models.CharField(verbose_name="Short description for promo",
+                                  blank=True,
+                                  max_length=255)
     price = models.DecimalField(max_digits=9,
                                 decimal_places=2,
                                 verbose_name="Price")
@@ -53,6 +55,7 @@ class Product(models.Model):
                                         verbose_name="Stok")
 
     available = models.BooleanField(default=True)
+    on_front = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
@@ -65,3 +68,69 @@ class Product(models.Model):
         return reverse('catalog:product_detail',
                        kwargs={"category_slug": self.category.slug,
                                "product_slug": self.slug})
+
+
+class Customer(models.Model):
+    user = models.ForeignKey(User,
+                             verbose_name='Username',
+                             on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15,
+                             verbose_name='Phone number',
+                             null=True,
+                             blank=True)
+    address = models.CharField(max_length=255,
+                               verbose_name='Address',
+                               null=True,
+                               blank=True)
+    orders = models.ManyToManyField('Order',
+                                    verbose_name='Orders',
+                                    related_name='related_order')
+
+    def __str__(self):
+        return "{} {}".format(self.user.first_name, self.user.last_name)
+
+
+# class Order(models.Model):
+#     STATUS_NEW = 'new'
+#     STATUS_IN_PROGRESS = 'in_progress'
+#     STATUS_READY = 'is_ready'
+#     STATUS_COMPLETED = 'completed'
+#
+#     BUYING_TYPE_SELF = 'self'
+#     BUYING_TYPE_DELIVERY = 'delivery'
+#
+#     STATUS_CHOICES = (
+#         (STATUS_NEW, 'Новый заказ'),
+#         (STATUS_IN_PROGRESS, 'Заказ в обработке'),
+#         (STATUS_READY, 'Заказ готов'),
+#         (STATUS_COMPLETED, 'Заказ выполнен')
+#     )
+#
+#     BUYING_TYPE_CHOICES = (
+#         (BUYING_TYPE_SELF, 'Самовывоз'),
+#         (BUYING_TYPE_DELIVERY, 'Доставка')
+#     )
+#
+#     customer = models.ForeignKey(Customer,
+#                                  verbose_name='Customer',
+#                                  related_name='related_orders',
+#                                  on_delete=models.CASCADE)
+#
+#     cart = models.ForeignKey(Cart, verbose_name='Корзина', on_delete=models.CASCADE, null=True, blank=True)
+#     status = models.CharField(
+#         max_length=100,
+#         verbose_name='Статус заказ',
+#         choices=STATUS_CHOICES,
+#         default=STATUS_NEW
+#     )
+#     buying_type = models.CharField(
+#         max_length=100,
+#         verbose_name='Тип заказа',
+#         choices=BUYING_TYPE_CHOICES,
+#         default=BUYING_TYPE_SELF
+#     )
+#     comment = models.TextField(verbose_name='Комментарий к заказу', null=True, blank=True)
+#     created_at = models.DateTimeField(auto_now=True, verbose_name='Дата создания заказа')
+#
+#     def __str__(self):
+#         return str(self.id)
