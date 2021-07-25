717ca14bde566e19dfc03248a1ebf75b5474f54a
chudomozg <chudomozg@gmail.com>
2021-07-25 14:28:58 +1000

    reate token generator for email confirmation

---
+++ users/tokens.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from six import text_type
+
+
+class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
+    # Token generator for activate user by email (email confirmation)
+    # We will use username and timestamp to prevent decode username from token
+    def _make_hash_value(self, user, timestamp):
+        return (
+            text_type(user.pk) + text_type(timestamp) +
+            text_type(user.is_active)
+        )
+
+
+account_activation_token = AccountActivationTokenGenerator()
